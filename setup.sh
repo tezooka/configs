119f4e5fb589036c49868d713dc4b76f39f07476
tezooka <yousuketezuka@gmail.com>
2022-11-26 13:40:48 +0900

    fix setup.sh

--- setup.sh
+++ setup.sh
@@ -5,6 +5,7 @@ if [ "$(uname)" == "Darwin" ] && [ "$(uname -m)" == "x86_64" ]; then
   echo "- x86_64 Mac"
 elif [ "$(uname)" == "Darwinn" ] && [ "$(uname -m)" == "arm64"]; then
   echo "- arm64 Mac"
+  zsh -c "$(curl -H 'Cache-Control: no-cache' -sfSL https://raw.githubusercontent.com/tezooka/configs/master/arm64-mac-setup.sh)"
 else
   echo "Your platform ($(uname -a)) is not supported."
 fi
