8beb3301bc7e33dd23fae69efe234c59259f2fbf
tezooka <yousuketezuka@gmail.com>
2022-11-26 14:24:02 +0900

    fix error

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 echo -e '----\nOS'
 if [ "$(uname)" == "Darwin" ] && [ "$(uname -m)" == "x86_64" ]; then
   echo "- x86_64 Mac"
-elif [ "$(uname)" == "Darwin" ] && [ "$(uname -m)" == "arm64"]; then
+elif [ "$(uname)" == "Darwin" ] && [ "$(uname -m)" == "arm64" ]; then
   echo "- arm64 Mac"
   zsh -c "$(curl -H 'Cache-Control: no-cache' -sfSL https://raw.githubusercontent.com/tezooka/configs/master/arm64-mac-setup.sh)"
 else
